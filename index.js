1f6f152abb0b5efdd01a71d97c228a5c870ead9f
Treagan Birbal <treaganbirbal@pursuit.org>
2019-08-09 11:03:51 -0400

    remove debugger and comments

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ document.addEventListener('DOMContentLoaded', () => {
   fetch('https://ghibliapi.herokuapp.com/films')
     .then(res => res.json())
     .then(res => {
-      // debugger;
       let body = document.querySelector('body');
       let select = document.createElement('select');
       body.appendChild(select)
@@ -51,34 +50,3 @@ document.addEventListener('DOMContentLoaded', () => {
    })
    .catch(err => err)
 });
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   let button = document.querySelector("button");
-//
-//   button.addEventListener("click", () => {
-//     let colorDiv = document.querySelector(".colorDiv");
-//     if (colorDiv.classList.value.includes("redClass")) {
-//       colorDiv.classList.remove("redClass");
-//       colorDiv.classList.add("greenClass");
-//     } else {
-//       colorDiv.classList.remove("greenClass");
-//       colorDiv.classList.add("redClass");
-//     }
-//   });
-//
-//   fetch("https://ghibliapi.herokuapp.com/films")
-//     .then(res => res.json())
-//     .then(res => {
-//       let body = document.querySelector("body");
-//       let ul = document.createElement("ul");
-//       body.appendChild(ul);
-//
-//       res.forEach(movieObj => {
-//         let li = document.createElement("li");
-//         let title = movieObj.title;
-//         li.innerText = title;
-//         ul.appendChild(li)
-//       })
-//
-//     })
-// });
